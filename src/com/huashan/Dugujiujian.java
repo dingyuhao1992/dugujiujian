dec073105eddb222410f52a97f4222abde16eb80
丁禹皓1991 <dingyuhao1991@126.com>
2017-03-02 10:57:07 +0800

    update02

--- src/com/huashan/Dugujiujian.java
+++ src/com/huashan/Dugujiujian.java
@@ -4,7 +4,8 @@ public class Dugujiujian {
 
 	public static void main(String[] args) {
 		System.out.println("hello ,dugujiujian");
-		
+		System.out.println("hello ,dugujiujian");
+		System.out.println("hello ,dugujiujian");
 	}
 
 }
